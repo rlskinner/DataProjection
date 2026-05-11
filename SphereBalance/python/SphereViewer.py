b47cbb457e923cf33e2223b20fd71288462c8d8a
rlskinner <rlskinner@yahoo.com>
2026-05-10 18:22:08 -0700

    Add a wireframe sphere to our view

--- SphereBalance/python/SphereViewer.py
+++ SphereBalance/python/SphereViewer.py
@@ -15,49 +15,59 @@ class SphereViewer:
         w.show()
         w.setWindowTitle("Sphere Points")
 
-        w.setCameraPosition(distance=10)
+        w.setCameraPosition(distance=3.0)  # Set the camera distance to view the sphere
 
         axis = pgl.GLAxisItem()
         w.addItem(axis)
-        w.addItem(SphereWireframeItem())  # Add the wireframe sphere to the view
+
+        wireframe = SphereWireframeItem(latitude_steps=9, longitude_steps=12)
+        w.addItem(wireframe)
 
         pg.exec()
 
 class SphereWireframeItem(pgl.GLGraphicsItem.GLGraphicsItem):
-    def __init__(self,  parentItem: 'GLGraphicsItem' = None, latitude_steps=16, longitude_steps=8):
+    def __init__(self,  parentItem: 'GLGraphicsItem' = None, latitude_steps=17, longitude_steps=4*9):
         super().__init__()
-        self.sphereMesh = np.zeros((latitude_steps, longitude_steps, 3), dtype=np.float32)
 
+        self.sphereMesh = np.zeros((latitude_steps, longitude_steps, 3), dtype=np.float32)
         for i in range(latitude_steps):
-            lat = np.radians(-90 + i * 180 / (latitude_steps - 1))
+            lat = np.radians(i*180/(latitude_steps-1))
             for j in range(longitude_steps):
-                lon = np.radians(j * 360 / longitude_steps)
+                lon = np.radians(j*360/longitude_steps)
                 x = np.cos(lon) * np.sin(lat)
                 y = np.sin(lon) * np.sin(lat)
                 z = np.cos(lat)
                 self.sphereMesh[i, j] = [x, y, z]
 
+                # print(f"Vertex ({i}, {j}): {self.sphereMesh[i, j]}")
+        pass
 
     def paint(self):
+        # Old OpenGL code is deprecated!
+        ogl.glMatrixMode(ogl.GL_MODELVIEW)
+        ogl.glLoadMatrixf(self.modelViewMatrix().data())
+        ogl.glMatrixMode(ogl.GL_PROJECTION)
+        ogl.glLoadMatrixf(self.projectionMatrix().data())
+        
         ogl.glColor3f(0.0, 1.0, 1.0)  # cyan color
 
         # Draw latitude lines
         for i in range(self.sphereMesh.shape[0]):
-            ogl.glBegin(ogl.GL_LINE_STRIP)
+            ogl.glBegin(ogl.GL_LINE_LOOP)
             for j in range(self.sphereMesh.shape[1]):
+                v = self.sphereMesh[i, j]
                 ogl.glVertex3f(*self.sphereMesh[i, j])
-            ogl.glVertex3f(*self.sphereMesh[i, 0])
             ogl.glEnd()
 
         # Draw longitude lines
         for j in range(self.sphereMesh.shape[1]):
             ogl.glBegin(ogl.GL_LINE_STRIP)
             for i in range(self.sphereMesh.shape[0]):
-                ogl.glVertex3f(*self.sphereMesh[i, j])
-            ogl.glVertex3f(*self.sphereMesh[0, j])
+                v = self.sphereMesh[i, j]
+                ogl.glVertex3f(*v)
             ogl.glEnd()
         
-        ogl.glFlush()
+        self.update()
 
 
     
